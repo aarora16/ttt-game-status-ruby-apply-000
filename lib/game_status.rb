b446fd85a269fc027689cefff0e34a6416fb9491
Anshul Arora <a.99.anshul@gmail.com>
2020-09-03 19:29:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  
+  ]
 def won?(board, index)
-  case board[index]
+  
   when 
 end
