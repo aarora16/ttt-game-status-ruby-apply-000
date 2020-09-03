16bd7a715b828f434e03efeb84df4163d728ba18
Anshul Arora <a.99.anshul@gmail.com>
2020-09-03 19:40:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,5 +15,19 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 def won?(board)
-  
+  for each combination in WIN_COMBINATIONS
+    ind_1 = combination[0]
+    ind_2 = combination[1]
+    ind_3 = combination[2]
+    
+    pos_1 = board[ind_1]
+    pos_2 = board[ind_2]
+    pos_3 = board[ind_3]
+    
+    if pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
+      combination
+    else
+      false
+    end
+  end
 end
